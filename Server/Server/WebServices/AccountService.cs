561fda3dd541bf89dc67bedb03e336352c48be10
Andrew Borland <warofasgard@gmail.com>
2015-07-31 11:26:24 +0100

    Add missing files.

---
+++ Server/Server/WebServices/AccountService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.Web.Script.Serialization;
+using System.IO;
+using Orleans;
+using Orleans.Runtime;
+using System.Net.Http;
+using System.ServiceModel.Channels;
+
+namespace Server
+{
+    [ServiceContract]
+    public interface IAccountService
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/auth/{account}/{password}")]
+        Message Auth(string account, string password);
+    }
+
+    public class AccountService : IAccountService
+    {
+        public Message Auth(string account, string password)
+        {
+            if (Orleans.GrainClient.IsInitialized == false)
+                return WebService.JSONMessage(new { error = "Client not initialised" });
+
+            if (account == null) //should never be hit provided no-one touches the templates
+                return WebService.JSONMessage(new { error = "Account name must be provided" });
+            if (password == null) //should never be hit provided no-one touches the templates
+                return WebService.JSONMessage(new { error = "Password must be provided" });
+
+            IAccountGrain ac = Orleans.GrainClient.GrainFactory.GetGrain<IAccountGrain>(account);
+
+            if (ac == null)
+                return WebService.JSONMessage(new { error = "Internal error: client returned null actor" });
+
+            var result = ac.Authenticate(password).Result;
+
+            var ret = new
+            {
+                account = account,
+                auth_result = result,
+                auth_result_enum = result.ToString()
+            };
+
+            return WebService.JSONMessage(ret);
+        }
+    }
+}
+
---
+++ Server/Server/WebServices/Run.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+using System.IO;
+using Orleans;
+using Orleans.Runtime;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Script.Serialization;
+using System.ServiceModel.Channels;
+
+namespace Server
+{
+    class WebService
+    {
+        public object Json { get; private set; }
+
+        public static void Run()
+        {
+            //if (System.IO.File.Exists("Config-Web.xml"))
+            {
+                if (!System.IO.File.Exists("Config-Client.xml"))
+                    throw new InvalidOperationException("WebService cannot start without client support");
+
+                //start web server :)
+                WebServiceHost host = new WebServiceHost(typeof(Service), new Uri("http://localhost.com:9000/")); //todo: move to config
+                ServiceEndpoint endpoint = host.AddServiceEndpoint(typeof(IService), new WebHttpBinding(), "service");
+                host.Open();
+
+                WebServiceHost host2 = new WebServiceHost(typeof(AccountService), new Uri("http://localhost.com:9000/")); //todo: move to config
+                ServiceEndpoint endpoint2 = host2.AddServiceEndpoint(typeof(IAccountService), new WebHttpBinding(), "account");
+                host2.Open();
+            }
+        }
+
+        public static HttpResponseMessage ToJSON(object responseModel)
+        {
+            HttpResponseMessage response = new HttpResponseMessage();
+
+            JavaScriptSerializer s = new JavaScriptSerializer();
+
+            string jsonClient = s.Serialize(responseModel);
+            byte[] resultBytes = Encoding.UTF8.GetBytes(jsonClient);
+            response.Content = new StreamContent(new MemoryStream(resultBytes));
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+
+            return response;
+        }
+
+        public static Message JSONMessage(object response)
+        {
+            JavaScriptSerializer s = new JavaScriptSerializer();
+
+            string jsonClient = s.Serialize(response);
+
+            return WebOperationContext.Current.CreateTextResponse(jsonClient, "text/plain", Encoding.UTF8);
+        }
+    }  
+}
