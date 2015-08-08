74160d55f6c8c862af3ae3bf4588964b2e2f4d92
Andrew Borland <warofasgard@gmail.com>
2015-08-09 00:11:01 +0100

    Not sure why VS didn't add this, but it's missing.

---
+++ Server/Grains/Session/Handlers/AccountDataHandlers.cs
@@ -0,0 +1,42 @@
+﻿using Orleans;
+using Orleans.Concurrency;
+using Orleans.Providers;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans.Streams;
+using Shared;
+using System.Collections;
+
+namespace Server
+{
+    public partial class Session
+    {
+        public async Task HandleReadyForAccountDataTimes()
+        {
+            if (!IsAuthedRealmSession())
+                return;
+
+            await Account.SendAccountDataTimes(0x15);
+        }
+
+        public async Task HandleUpdateAccountData(UInt32 type, UInt32 time, UInt32 size, byte[] data)
+        {
+            if (!IsAuthedRealmSession())
+                return;
+            await Account.UpdateAccountData(type, time, size, data);
+        }
+
+        public async Task HandleRequestAccountData(UInt32 type)
+        {
+            if (!IsAuthedRealmSession())
+                return;
+            await Account.SendAccountData(type);
+        }
+    }
+}
